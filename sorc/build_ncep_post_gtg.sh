7d2ca8ba6a5b10eb2cb8f3f9c1a21e851ad7a413
Yali Mao <yali.mao@noaa.gov>
2021-06-15 17:35:20 +0000

    Remodelled the work directories

--- sorc/build_ncep_post_gtg.sh
+++ sorc/build_ncep_post_gtg.sh
@@ -2,16 +2,17 @@ set -x
 cwd=`pwd`
 cp ncep_post_gtg.fd/*90 ncep_post.fd/.
 
-cd $HOMEgit/EMC_post_wafs
+cd $HOMEgit/EMC_post.wafs
 git submodule update --init CMakeModules
-cd $HOMEgit/EMC_post_wafs/tests
+cd $HOMEgit/EMC_post.wafs/tests
 ./compile_upp.sh
 
 
-cd $cwd/ncep_post_gtg.fd
-files=`ls *90`
+cd $cwd/ncep_post_gtg_stub.fd
+files=`ls`
 
 cd $cwd
-for file in $files ; do
-    cp ncep_post_gtg.fd/${file}.stub ncep_post.fd/$file
+for filestub in $files ; do
+    afile=`echo "${filestub%.*}"`
+    cp ncep_post_gtg_stub.fd/$filestub ncep_post.fd/$afile
 done
